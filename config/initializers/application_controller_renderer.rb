0c9780eebd0e6c701ed28608314e2d356b7370da
ryoot <ryota.bsk.1202@gmail.com>
2020-10-08 23:01:53 +0900

    ユーザー機能の完成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0fd360c20d3bb8fb497db5f5b11a27caccb8963da1d49af252efb0f7fa3958dd544f3442d619c216313cb6f8d83624cd1466bfc3d58b0a9f558b21e8084cebce'
